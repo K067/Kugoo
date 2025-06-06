d130297d51dfd3c7cca39c8cae224a53ad27028a
K067 <74493829+K067@users.noreply.github.com>
2025-06-06 14:29:17 +0200

    Update handler.php

--- handler.php
+++ handler.php
@@ -2,8 +2,8 @@
 $user_email=htmlspecialchars($_POST["user_email"]);
 $user_phone=htmlspecialchars($_POST["user_phone"]);
 
-$token = "6144473339:AAGEvoLd26LjgExd0hPEkxpUJig5GSLDBA8";
-$chat_id = "-993149160";
+$token = "";
+$chat_id = "";
 
 $formData = array(
    "email: " => $user_email ,
@@ -14,7 +14,7 @@ foreach($formData as $key => $value){
     $text .=$key."<b>".urlencode($value)."</b>"."%0A";
 }
 
-$sendToTelegram=fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$text}&parse_mode=html","r");
+$sendToTelegram=fopen("","r");
 
 if($sendToTelegram){
     echo "Ok";
